1d2941a6105d18694ee03a8f712454da6550cb3d
Erik Squires <erik.squires@gmail.com>
2025-10-01 12:38:02 -0400

    Renaming scripts with more descriptive and shorter names.  Minor comment improvements in main.R, along with breaking up quarto rendering by report type.

--- R/main.R
+++ R/main.R
@@ -3,8 +3,10 @@
 # Author: Erik K. Squires
 # Date: 2025-09-20
 #
-
 # Run from project root. 
+#
+#
+
 source_here <- function(fname) {
   source(here::here("R", fname))
 }
@@ -13,13 +15,13 @@ source_here <- function(fname) {
 unlink(here::here("data", "*.csv"))
 
 #
-# Required to be sequential.
+# Required to be run sequentially.
 #
-source_here("process_excel.R")
-source_here("summarize_vaes_csv_by_symptom_domain.R")
+source_here("excel_to_csv.R")
+source_here("sum_csv_by_symptom_domain.R")
 
 #
-# These can be run any time afterwards.
+# These can be run in any order afterwards.
 #
 source_here("ancova_analysis.R")
 source_here("leave_one_out.R")
@@ -33,8 +35,11 @@ source_here("r2_by_domain.R")
 # Render documents.
 #
 q_doc <- here::here("docs", "VaesSubgroups.qmd")
-quarto::quarto_render(q_doc, output_format = "pdf")
-quarto::quarto_render(q_doc, output_format = "html")
 
-# Not usually needed.  
+quarto::quarto_render(q_doc)
+
+# quarto::quarto_render(q_doc, output_format = "pdf")
+# quarto::quarto_render(q_doc, output_format = "html")
+
+# Force cache refresh.  Not usually needed.  
 # quarto::quarto_render(here::here("docs", "VaesSubgroups.qmd"), cache_refresh = TRUE)
